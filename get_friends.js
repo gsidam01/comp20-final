17cb3087b764784c11d9b899beaec2d9baf9c29d
milesizydorczak12 <56458475+milesizydorczak12@users.noreply.github.com>
2020-04-30 17:19:46 -0400

    Update get_friends.js

--- get_friends.js
+++ get_friends.js
@@ -2,12 +2,12 @@ function insert_friends_data(returned_from_server)
 {
 	var ret_obj = returned_from_server;
 	// var ret_obj = JSON.parse(returned_from_server); my local variable is already json, otherwise it would need to be parsed
-	var insert_data = "<tr align='left'><th>Friends</th><th>Location</th><th>Time Leaving</th></tr>";
+	var insert_data = "<tr><th>Friends</th><th>Location</th><th>Time Leaving</th></tr>";
 	for (let i = 0; i < ret_obj.friends.length; i++) {
 		insert_data += "<tr>" + "<td>" + ret_obj.friends[i].name + "</td>"
 					+ "<td>" + ret_obj.friends[i].location + "</td>"
 					+ "<td>" + ret_obj.friends[i].time_end + "</td>" + "</tr>";
 	}
 	document.getElementById("friends_pop_here").innerHTML = insert_data;
-	document.getElementsById("no_user_signed_in").innerHTML = "";
+	document.getElementById("no_user_signed_in").innerHTML = "";
 }
