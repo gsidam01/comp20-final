e37283110b2731d14b68bb22f8d30448c14e63c5
ryanghayour <56457626+ryanghayour@users.noreply.github.com>
2020-04-30 18:43:07 -0400

    Update get_time.js

--- get_time.js
+++ get_time.js
@@ -1,31 +1,34 @@
-function get_time(){
+function get_time(duration_hours_string, duration_minutes_string, time_hours_string, time_minutes_string, am){
+  let duration_hours = parseInt(duration_hours_string);
+  let duration_minutes = parseInt(duration_minutes_string);
+  let time_hours = parseInt(time_hours_string);
+  let time_minutes = parseInt(time_minutes_string);
   let today = new Date();
   let current_hours = today.getHours();
   let current_minutes = today.getMinutes();
-  let duration_hours = getElementById(duration_hours);
-  let duration_minutes = getElementById(duration_minutes);
-  let time_hours = getElementById(time_hours);
-  let time_minutes = getElementById(time_minutes);
   if (time_hours > 12){
     console.log("Error, hours must be in am/pm format\n");
-  //if (pm)
+  if (!am){
   time_hours += 12;
-  // if duration_hours == undefined {
-  if (current_hours > time_hours) {
-    duration_hours = time_hours + 24 - current_hours;
+  }
+  if (duration_hours == "") {
+    if (current_hours > time_hours) {
+      duration_hours = time_hours + 24 - current_hours;
+    }
+    else{
+      duration_hours = time_hours - current_hours;
+    }
+    if (current_minutes > time_minutes) {
+      duration_minutes = time_minutes + 60 - current_minutes;
+    }
+    else{
+      duration_minutes = time_minutes - current_minutes;
+    }
   }
   else{
-    duration_hours = time_hours - current_hours;
+    if (!am){
+      duration_hours += 12;
+    }
   }
-  if (current_minutes > time_minutes) {
-    duration_minutes = time_minutes + 60 - current_minutes;
-  }
-  else{
-    duration_minutes = time_minutes - current_minutes;
-  }
-  //}
-  // else{
-    duration_hours += 12;
-  //}
-  return duration_hours, duration_minutes;
-  }
+  return (duration_hours + ":" + duration_minutes);
+}
