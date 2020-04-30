b197bda443a5238909f930b5f47f229c2da02a7c
milesizydorczak12 <56458475+milesizydorczak12@users.noreply.github.com>
2020-04-30 12:50:04 -0400

    starting xxmlhttprequest

--- miles_gapi_test.js
+++ miles_gapi_test.js
@@ -1,10 +1,76 @@
-var http = require('http');
-http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.write("<script src=\"https://apis.google.com/js/platform.js\" async defer></script>");
-   res.write("<title> Google API test </title> <meta charset=\"utf-8\">");
-   res.write("<meta name=\"google-signin-client_id\" content=\"626801578575-q0vuggp5r43sl9v79qhv7ksmhgo19p4u.apps.googleusercontent.com\">");
-   res.write("<div class=\"g-signin2\" data-longtitle=\"true\" data-width=\"240\" data-height=\"50\"data-onsuccess=\"onSignIn\"></div>");
+const http = require('http');
+const url = require("url");
 
-   res.end("hello!");
-}).listen(8080); 
+
+var httpServer = http.createServer(function (req, res) {
+        switch (req.method) {
+            case "GET":
+                console.log("Request url" + req.url);
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.write("A get method was requested");
+
+                res.end("ended response.");
+                break;
+            case "POST":
+                console.log("Request url" + req.url);
+                break;
+            default:
+                break;
+        }
+
+});
+
+httpServer.listen(8080); 
+
+
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const mongo_url = "mongodb+srv://MilesIzydorczak:ashenmoorliege99@cluster0-sx3lm.mongodb.net/test?retryWrites=true&w=majority";
+// const url = require("url");
+// const http = require("http");
+
+// function write_stuff(res, item, call_back) {
+//     res.write("The company is: " + item.Company + "<br>");
+//     res.write("The ticker is: " + item.Ticker + "<br>");
+//     call_back();
+// }
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     var qobj = url.parse(req.url, true).query;
+//     var request_type = qobj.request_type;
+//     var searched_text = qobj.text_field;
+//     console.log(searched_text);
+//     MongoClient.connect(mongo_url, { useUnifiedTopology: true }, function(err, db) {  
+//         if (err) { 
+//             console.log ("Error: " + err); 
+//             return; 
+//         }   
+
+//         var database_object = db.db("companies");
+//         var collection = database_object.collection('companies');
+
+//         if (request_type == "company") {
+//             var result_obj = collection.find({Company : searched_text}, 
+//                 {projection: {_id : 0, Ticker : 1, Company : 1}});
+//             result_obj.on("data", function(item) {
+//                 console.log(item.Ticker);
+//                 console.log(item.Company);
+//                 write_stuff(res, item, function() {
+//                     res.end();
+//                 });
+//             });
+//         } else {
+//             var result_obj = collection.find({Ticker : searched_text}, 
+//                             {projection: {_id : 0, Ticker : 1, Company : 1}});
+//             result_obj.on("data", function(item) {
+//                 console.log(item.Ticker);
+//                 console.log(item.Company);
+//                 write_stuff(res, item, function() {
+//                     res.end();
+//                 });
+//             });
+//         }
+//     });
+// }).listen(8080);
