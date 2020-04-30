2eaf44dc501438189d11e81dd88165570bfeb7aa
ryanghayour <56457626+ryanghayour@users.noreply.github.com>
2020-04-30 17:33:14 -0400

    Update get_time.js

--- get_time.js
+++ get_time.js
@@ -4,4 +4,28 @@ function get_time(){
   let current_minutes = today.getMinutes();
   let duration_hours = getElementById(duration_hours);
   let duration_minutes = getElementById(duration_minutes);
-  let 
+  let time_hours = getElementById(time_hours);
+  let time_minutes = getElementById(time_minutes);
+  if ((duration_hours > 12) || (time_hours > 12)){
+    console.log("Error, hours must be in am/pm format\n");
+  //if (pm)
+  time_hours += 12;
+  // if duration_hours == undefined {
+  if (current_hours > time_hours) {
+    duration_hours = time_hours + 24 - current_hours;
+  }
+  else{
+    duration_hours = time_hours - current_hours;
+  }
+  if (current_minutes > time_minutes) {
+    duration_minutes = time_minutes + 60 - current_minutes;
+  }
+  else{
+    duration_minutes = time_minutes - current_minutes;
+  }
+  //}
+  // else{
+    duration_hours += 12;
+  //}
+  return duration_hours;
+  }
